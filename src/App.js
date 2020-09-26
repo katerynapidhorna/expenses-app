3caa5a76b857485c692a8ab1e7716dca0f49bd04
katerynapidhorna <katerynapidhorna@gmail.com>
2020-09-26 12:40:31 +0200

    add create-expense route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Expenses from "./pages/Expenses";
+import CreateExpense from "./pages/CreateExpense";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/expenses" component={Expenses} />
+        <Route path="/create-expense" component={CreateExpense} />
       </Switch>
     </div>
   );
---
+++ src/pages/CreateExpense.js
@@ -0,0 +1,5 @@
+import React from "react";
+console.log(localStorage.getItem("data"));
+export default function CreateExpense() {
+  return <div>Create expense</div>;
+}
